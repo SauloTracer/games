f5374b5844d898ede78cfe61bac336d140d228e3
SauloMachado <saulo.tracer@gmail.com>
2025-02-27 14:38:50 -0300

    Corrigindo estilo e ação do botão

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,7 +19,7 @@
             Sudoku
             <button
                 class="button"
-                @click="showNewGameDialog = true"
+                @click="this.$router.push('/sudoku')"
             >
                 <v-icon>mdi-play</v-icon>
             </button>
@@ -36,4 +36,31 @@ const isMobileStore = useIsMobileStore();
 const isMobile = computed(() => isMobileStore.isMobile);
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.button {
+    appearance: none;
+    background-color: #FAFBFC;
+    border: 1px solid rgba(27, 31, 35, 0.15);
+    border-radius: 6px;
+    box-shadow: rgba(27, 31, 35, 0.04) 0 1px 0, rgba(255, 255, 255, 0.25) 0 1px 0 inset;
+    box-sizing: border-box;
+    color: #24292E;
+    cursor: pointer;
+    display: inline-block;
+    font-family: -apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 20px;
+    list-style: none;
+    padding: 6px 10px;
+    margin: 4px;
+    position: relative;
+    transition: background-color 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    vertical-align: middle;
+    white-space: nowrap;
+    word-wrap: break-word;
+}
+</style>

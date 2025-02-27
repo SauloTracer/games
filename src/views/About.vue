11edf4f9d71a7fff1e67393066b5a6884e6b8785
SauloMachado <saulo.tracer@gmail.com>
2025-02-27 14:25:32 -0300

    Adicionando link para o sudoku na home.

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,12 +15,23 @@
             style="width: 100%;"
         />
         <p>Made by <a href="mailto:saulo.tracer@gmail.com">Tr@c3r</a></p>
+        <Title>
+            Sudoku
+            <button
+                class="button"
+                @click="showNewGameDialog = true"
+            >
+                <v-icon>mdi-play</v-icon>
+            </button>
+        </Title>
     </div>
 </template>
 
 <script setup lang='ts'>
 import { computed } from 'vue'
 import { useIsMobileStore } from '@/stores/isMobile';
+import Title from '../components/Title.vue';
+
 const isMobileStore = useIsMobileStore();
 const isMobile = computed(() => isMobileStore.isMobile);
 </script>
